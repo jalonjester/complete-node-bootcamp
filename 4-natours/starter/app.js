0f09a739c2d2a381589baf8741d2787f6eae1b2a
etimusma <eliot.timus-mace.external@capgemini.com>
2025-03-24 13:28:23 +0100

    [UPDATE] finish chapter 6

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
-
-
 // 1) MIDDLEWEARS
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the server')
+  console.log('Hello from the server');
   next();
 });
 
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,10 @@
-const app = require(`${__dirname}/app.js`)
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const app = require(`${__dirname}/app.js`);
+
+console.log(process.env);
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Ruinning on port ${port}...`);
 });
